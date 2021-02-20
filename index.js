50062c451c7050ec1181a2d0d179812535938152
HORRIBLE12 <albert_destroyer@protonmail.com>
2021-02-20 19:12:50 +0000

    done

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var sum = 0;
 function addThree() {
     sum = sum + 3
 }
-
 // Only change code below this line
-
-
+ function addFive(){
+     var foot=sum+5
+    return foot;
+ }
 // Only change code above this line
 
 addThree() //this adds three to a number but returned value is undefined
